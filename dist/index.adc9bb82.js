a14cb96e3033fefcd964be6206d373a7599e2658
Labhrás Mac Gabhann <coding@larrymg.me>
2021-08-23 13:31:29 -0400

    feat: Run Parcel build to produce finished, optimized landing page.

---
+++ dist/index.adc9bb82.js
@@ -0,0 +1,2 @@
+function togglePassword(){let e=document.getElementById("password"),s=document.getElementById("eye_show_password"),t=document.getElementById("eye_hide_password");"password"===e.type?(e.type="text",t.classList.toggle("hide"),s.classList.toggle("hide")):(e.type="password",t.classList.toggle("hide"),s.classList.toggle("hide"))}
+//# sourceMappingURL=index.adc9bb82.js.map
